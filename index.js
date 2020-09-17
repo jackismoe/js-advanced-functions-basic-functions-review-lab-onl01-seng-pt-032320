63e8bf9e7a83a4d4a534aa06b6f99316b606da1c
jackismoe <jacksonmo@me.com>
2020-09-17 11:07:43 -0400

    Lab Submit

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+const saturdayFun = (activity = 'roller-skate') => {
+  return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = (work = 'go to the office') => {
+  return `This Monday, I will ${work}.`
+}
+
+const wrapAdjective = (flair = '*') => {
+  return (msg = 'special') => {
+    return `You are ${flair}${msg}${flair}!`
+  }
+}
+
+const Calculator = {
+  add: () => {
+    return 1 + 3
+  },
+  subtract: () => {
+    return 1 - 3
+  },
+  multiply: () => {
+    return 1 * 3
+  },
+  divide: () => {
+    return 10 / 5
+  }
+}
+
+const actionApplyer = (start, ray) => {
+  let a = start
+
+  for (let i = 0; i < ray.length; i++) {
+    a = ray[i](a)
+  }
+  return a
+}
